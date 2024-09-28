7b2d8bf3039a9dc161ea5a5f594d6f7ab5f89bf6
sabaputuridze <puturidzesaba01@gmail com>
2024-09-28 08:52:47 -0700

    5th commit

---
+++ level 6/classwork.py
@@ -0,0 +1,20 @@
+name = input ("Enter your name:")
+print("Hello"+name)
+
+
+num1=6
+num2=1
+
+print(num1+num2)
+
+salary1 = int (input("Enter salary of first person"))
+salary2 = int (input("Enter salary of second person"))
+
+print (salary1+salary2)
+
+
+num1=8
+num2=8
+print(num1*num2)
+
+
