8265e61d27920e02c6a5df80481f25ff08a9cdad
sabaputuridze <puturidzesaba01@gmail com>
2024-09-28 08:59:51 -0700

    6th commit

---
+++ day 2/lesson 2/lesson 2.py
@@ -0,0 +1,15 @@
+name = "saba"
+surename = "puturidze"
+age = "11"
+
+mom_name = "nini"
+mom_surename = "kochlamazashvili"
+non_age = "34"
+
+brother_name = "sandro"
+brother_surename = "puturidze"
+brother_age = "15" 
+
+dad_name = "beka"
+dad_surename = "puturidze"
+dad_age = "39"
