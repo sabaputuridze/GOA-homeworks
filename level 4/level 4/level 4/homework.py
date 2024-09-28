c58c92b02579f9f2ff4940afa5536cbea771c3ae
sabaputuridze <puturidzesaba01@gmail com>
2024-09-28 09:37:43 -0700

    7th commit

---
+++ level 4/level 4/level 4/homework.py
@@ -0,0 +1,20 @@
+points = 50
+points = 40  #პოინთის გადაკეთება
+
+print(points)
+
+lage = 156
+print("lage")  #არასწორი კოდის სწორად გადაკეთება
+
+
+
+
+num1 =1.5
+num2 =2.5
+print(num1+num2)
+print(num2-num1)   #რიცხვების მიმატება,გამოკლდება,გამრავლება
+print(num1*num2)
+print(num2//num1)
+
+#pirnt(number)
+print("number")   #არასწორი კოდის სწორად გადაკეთება
