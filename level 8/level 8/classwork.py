ea11f8cc1adfe89585ff95710c8aea7c5842455a
sabaputuridze <puturidzesaba01@gmail com>
2024-10-04 05:37:20 -0700

    9th commit

---
+++ level 8/level 8/classwork.py
@@ -0,0 +1,9 @@
+#intenger - არის მთელი რიცხვი მაგ:5,10,15
+#string არის პრჭყალები მაგ: "homework" "classwork"
+#float არის წილადი რიცხვი მაგ: 5.3 5.9
+#boolean არის True/false
+
+#concatenation არის ორი სიტყვის შეჯამება,შეწეპება მაგ:
+a = "hello"
+b = "world"
+print(a+b)
