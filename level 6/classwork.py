2247d15dcc8144897629a0035620be32912a045f
sabaputuridze <puturidzesaba01@gmail com>
2024-10-08 13:23:56 -0700

    11th commit

--- level 6/classwork.py
+++ level 6/classwork.py
@@ -10,11 +10,6 @@ print(num1+num2)
 salary1 = int (input("Enter salary of first person"))
 salary2 = int (input("Enter salary of second person"))
 
-print (salary1+salary2)
-
-
-num1=8
-num2=8
-print(num1*num2)
+print (salary1+salary2) 
 
 
