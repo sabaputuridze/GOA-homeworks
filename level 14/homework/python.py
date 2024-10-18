a526ee467bad7db9a806760b94fd2a4387b95c6e
sabaputuridze <puturidzesaba01@gmail com>
2024-10-18 07:50:27 -0700

    16th commit

---
+++ level 14/homework/python.py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print("hello world")
+
+for i in range(10):
+    print("goodbye")
+
+for num in range(101):
+    print(num)
+
+
+x=0
+while x < 101:
+    print(x)
+    x=x +1
+
+x =16
+while x <57:
+    print(x)
+    x=x +1
+
+
+for i in range(49,52):
+    print(i)
