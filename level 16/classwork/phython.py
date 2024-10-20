60f3803bec8ebdd6606aeeecc553885e037f289a
sabaputuridze <puturidzesaba01@gmail com>
2024-10-20 13:12:15 -0700

    18th commit

---
+++ level 16/classwork/phython.py
@@ -0,0 +1,14 @@
+for i in range(0,200, 6):
+    print(i)
+
+
+    str1= "GoodBye World!"
+    for i in str1:
+        print(i)
+
+
+for i in range(1001):
+    print(i)
+
+
+
---
+++ level 16/homework/phython.py
@@ -0,0 +1,19 @@
+for i in range(1, 11):
+    print(i)
+
+x=0
+while x <11:
+    print(x)
+    x=x+1
+
+num=10
+if num %2==0:
+    print("This num is even") 
+
+
+num=86
+if num < 100 and num > 90:
+    print("You got A")
+elif num < 87 and num > 80:
+    print("You got B")
+
