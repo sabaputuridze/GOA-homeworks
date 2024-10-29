bcee099a721095f164025a0d5ce8d0db26fc1c8d
sabaputuridze <puturidzesaba01@gmail com>
2024-10-29 10:25:54 -0700

    20th commit

---
+++ level 18/classwork/phyton.py
@@ -0,0 +1,4 @@
+cart ={"banana", "apple", "orange", "lime", "coconut"}
+cart2 ={"cheese", "oil", "juice", "bread", "tomato"}
+colors={"yellow","blue","red","orange","green"}
+print (cart, {4})
---
+++ level 18/homework/python.py
@@ -0,0 +1,12 @@
+list1 =["2", "51", "11", "13", "51", "100"]
+print (list1)
+
+print (list1[-1])
+
+print(list1)
+list1[5] = "41"
+print(list1)
+
+print(list1)
+list1[4] = "71"
+print(list1)
