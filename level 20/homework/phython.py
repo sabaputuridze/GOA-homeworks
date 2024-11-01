3d50d97f1074c78c7b861f6456335be35cf8b21a
sabaputuridze <puturidzesaba01@gmail com>
2024-11-01 12:55:28 -0700

    22th commit

---
+++ level 20/homework/phython.py
@@ -0,0 +1,12 @@
+list1 = ["dog", "cat", "owl", "tiger", "lion", "monkey", "bird", "turtle", "panda", "giraffe"]
+#          1      2      3       4       5        6        7        8         9        10 
+print (list1)
+
+print(list1[1:4])
+print(list1[7:10])
+print(list1[4:8])
+
+
+
+x="hello"
+print (x[::-1])
