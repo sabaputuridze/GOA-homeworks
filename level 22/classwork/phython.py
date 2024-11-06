2c148cf0f33e3e5150c8f3e41b8056ef04f70d00
sabaputuridze <puturidzesaba01@gmail com>
2024-11-06 08:45:26 -0800

    24th commit

---
+++ level 22/classwork/phython.py
@@ -0,0 +1,22 @@
+def goodbye():
+    print("Goodbye world")
+goodbye()
+
+
+def goodbye1(name):
+    print("Goodbye, " +name)
+    goodbye1 ("saba")
+    print(goodbye1)
+
+
+
+def summation(a, b):
+    print(a+b)
+summation(5,6)
+
+
+
+def output3 (list1):
+    print(list1[:3])
+
+output3([4,7,10,14,20,25,30])
---
+++ level 22/homework/phython.py
@@ -0,0 +1,9 @@
+age = 16
+if age>=18:
+    print("you got discount")
+else:
+    print("you didnt got discount")
+
+
+x="hello"
+print(x[::-1])
