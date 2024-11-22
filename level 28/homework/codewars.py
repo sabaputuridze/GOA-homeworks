f7307f7a5a5f27ebbf83bd9c5d48e5a403789920
sabaputuridze <puturidzesaba01@gmail com>
2024-11-22 12:40:21 -0800

    commit

---
+++ level 28/homework/codewars.py
@@ -0,0 +1,8 @@
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+def no_space(x):
+    return x.replace(' ' ,'')
+
+def boolean_to_string(b):
+    return str(b)
---
+++ level 6/homework/level 6/homework.py
@@ -0,0 +1,28 @@
+num1 = 700
+num2 = 800
+num3 = 400
+num4 = 411
+num5 = 765
+num6 = 900
+num7 = 144
+num8 = 777
+num9 = 111
+num10 =541
+
+print(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
+
+
+word1 = input ("chair")
+word2 = input ("table")
+word3 = input ("carpet")
+(word1+word2+word3)
+
+
+number1 = int (input("Enter your number:"))
+number2 = int (input("Enter your number:"))
+print (number1+number2)
+
+#Input არის როდესაც რაღაც შეგყავს კომპიუტერში
+#მაგალითად კლავიატურაზე როდესაც რაღაცას ვწერთ ის შედის კომპიუტერში
+#და შემდეგ ის გვანახებს როდესაც რაღაცას გვანახებს კომპიუტერი
+#მაგას output ქვია
